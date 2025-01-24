8e8466cd9c5bd94aadd501f6fd46bc1550177ca1
KentaF1202 <kentaf1202@gmail.com>
2025-01-24 17:18:43 -0500

    Version 1

--- LAMPAPI/EditContact.php
+++ LAMPAPI/EditContact.php
@@ -1,3 +1,55 @@
+<<<<<<< HEAD
+<?php
+    //Variables taken from js file
+    $inData = getRequestInfo();
+
+    $firstName = $inData["firstName"];
+    $lastName = $inData["lastName"];
+    $phone = $inData["phone"];
+    $email = $inData["email"];
+    $ID = $inData["ID"];
+
+    //Interactions with database
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+    if($conn->connect_error){
+        returnWithError("error: Could not connect to database");
+    }
+    else{
+        //Updates contact in database
+        $stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, 
+                                                    LastName = ?,
+                                                    Phone = ?,
+                                                    Email = ?
+                                                    WHERE ID = ?");
+        $stmt->bind_param("ssssi", $firstName, $lastName, $phone, $email, $ID);
+        $stmt->execute();
+        sendResultInfoAsJson('{"result":"Finished Successfully"}');
+
+        $stmt->close();
+        $conn->close();
+    }
+
+    //Gets input from file in key-value pairs
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    //Returns JSON object
+    function sendResultInfoAsJson($obj)
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    //Returns JSON error message
+    function returnWithError($err)
+    {
+        $retValue = '{"result":"' . $err . '"}';
+        sendResultInfoAsJson($retValue);
+    }
+?>
+=======
 <?php
     $inData = getRequestInfo();
     $firstName = $inData["firstName"];
@@ -40,3 +92,4 @@
         sendResultInfoAsJson($retValue);
     }
 ?>
+>>>>>>> 165bc0d86d552737ebc9d4f2f28e8f4069f48d7e
--- LAMPAPI/SignUp.php
+++ LAMPAPI/SignUp.php
@@ -1,4 +1,5 @@
 <?php
+	//Variables taken from js file
 	$inData = getRequestInfo();
 	
     $firstName = $inData["firstName"];
@@ -6,45 +7,66 @@
     $username = $inData["username"];
     $password = $inData["password"];
 
+	//Interactions with database
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
 	if( $conn->connect_error )
 	{
-		returnWithError( $conn->connect_error );
+		returnWithError("error: Could not connect to database");
 	}
+<<<<<<< HEAD
+	else
+	{
+		//Returns if usename already exists in database
+=======
 	else{
+>>>>>>> 165bc0d86d552737ebc9d4f2f28e8f4069f48d7e
 		$stmt = $conn->prepare("SELECT * FROM Users WHERE Username=?");
 		$stmt->bind_param("s", $username);
 		$stmt->execute();
 		$result = $stmt->get_result();
-
+		
 		if( $row = $result->fetch_assoc()  )
 		{
-			returnWithError("User Already Exists");
+			returnWithError("error: User Already Exists");
 		}
+<<<<<<< HEAD
+		else
+		{
+			//Inserts user into database
+			$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Username, Password) Values (?,?,?,?)");
+            $stmt->bind_param("ssss", $firstName, $lastName, $username, $password);
+            $stmt->execute();
+            sendResultInfoAsJson('{"result":"Finished Successfully"}');
+=======
 		else{
 		$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Username, Password) Values (?,?,?,?)");
 		$stmt->bind_param("ssss", $firstName, $lastName, $username, $password);
 		$stmt->execute();
 		returnWithError("Finished Successfully");
+>>>>>>> 165bc0d86d552737ebc9d4f2f28e8f4069f48d7e
 		}
+		
 		$stmt->close();
 		$conn->close();
 	}
 
+	//Gets input from file in key-value pairs
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
+	//Returns JSON object
 	function sendResultInfoAsJson( $obj )
 	{
 		header('Content-type: application/json');
 		echo $obj;
 	}
 	
+	//Returns JSON error message
 	function returnWithError( $err )
 	{
-		$retValue = '{"error":"' . $err . '"}';
+		$retValue = '{"result":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
 ?>
