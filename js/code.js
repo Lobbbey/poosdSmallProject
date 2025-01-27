afc553be935e5879ee6ccab87a50d472eb142671
Lobbbey <joshuatylerbandy@gmail.com>
2025-01-27 12:09:44 -0500

    updates to code.js and index

--- js/code.js
+++ js/code.js
@@ -1,47 +1,62 @@
 const urlBase = 'http://cop4331g11.xyz/LAMPAPI';
 const extension = 'php';
 
-function doLogin(){
-    userId = 0;
-    firstName = "";
-    lastName = "";
+let userId = 0;
+let firstName = "";
+let lastName = "";
 
-    let login = document.getElementById("loginName").value;
-    let password = document.getElementById("loginPassword").value;
 
-    document.getElementById("loginResult").innerHTML = "";
-    
-    let tmp = {login:login, password:password};
-    let jsonPayload = JSON.stringify(tmp);
+function doLogin()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	
+	let login = document.getElementById("loginName").value;
+	let password = document.getElementById("loginPassword").value;
+//	var hash = md5( password );
+	
+	document.getElementById("loginResult").innerHTML = "";
 
-    let url = urlBase + '/Login.' + extension;
-    
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-type", "application/json; charset = UTF-8");
-    try{
-        xhr.onreadstatechange = function(){
-            if(this.readyState == 4 && this.status == 200){
-                let jsonObject = JSON.parse(xhr.responseText);
-                userId = jsonObject.id;
-                
-                if(userId < 1){
-                    document.getElementById("loginResult").innerHTML = "Username or Password is incorrect";
-                    return;
-                }
+	let tmp = {login:login,password:password};
+//	var tmp = {login:login,password:hash};
+	let jsonPayload = JSON.stringify( tmp );
+	
+	let url = urlBase + '/Login.' + extension;
 
-                firstName = jsonObject.firstName;
-                lastName = jsonObject.lastName;
-                
-                saveCookie();
-                window.location.href = "Contact_list.html";
-            }
-        };
-        xhr.send(jsonPayload);
-    }
-    catch(err){
-        document.getElementById("loginResult").innerHTML = err.message;
-    }
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				let jsonObject = JSON.parse( xhr.responseText );
+				userId = jsonObject.id;
+		
+				if( userId < 1 )
+				{		
+					document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+					return;
+				}
+		
+				firstName = jsonObject.firstName;
+				lastName = jsonObject.lastName;
+
+				saveCookie();
+	
+				window.location.href = "contact_list.html";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
+
 }
 
 function saveCookie(){
@@ -84,6 +99,16 @@ function doLogout(){
     document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
     window.location.href = "index.html";
 }
+
+function addContact(){
+    let firstName = document.getElementById("
+
+}
+
+
+
+
+
 
 function foo() {
     console.log("FOO!");
