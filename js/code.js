7b60a07ccc2ed384343c85549907117767ad97cd
Joshua Bandy <joshuatylerbandy@gmail.com>
2025-02-05 22:14:24 -0500

    fixed adding a contact

--- js/code.js
+++ js/code.js
@@ -2,6 +2,8 @@ const urlBase = 'http://cop4331g11.xyz/LAMPAPI';
 const extension = 'php';
 
 let userId = 0;
+let firstName = "";
+let lastName = "";
 
 function doLogin()
 {
@@ -16,7 +18,7 @@ function doLogin()
 	document.getElementById("loginResult").innerHTML = "";
 
 	//let tmp = {username:login,password:password};
-	let tmp = {login:login,password: hash};
+	let tmp = {username:login,password: hash};
 	let jsonPayload = JSON.stringify( tmp );
 	
 	let url = urlBase + '/Login.' + extension;
@@ -31,7 +33,7 @@ function doLogin()
 			if (this.readyState == 4 && this.status == 200) 
 			{
 				let jsonObject = JSON.parse( xhr.responseText );
-				userId = jsonObject.userId;
+				userId = jsonObject.id;
                 console.log(userId);
 		
 				if( userId < 1 )
@@ -62,34 +64,36 @@ function saveCookie(){
     date.setTime(date.getTime()+(minutes*60*1000));
     document.cookie = 
         "firstName=" + firstName + 
-        ";lastName=" + lastName + 
-        ";userId=" + userId + 
+        ",lastName=" + lastName + 
+        ",userId=" + userId + 
         ";expires=" + date.toGMTString();
 }
 
-function readCookie(){
+function readCookie() {
     userId = -1;
     let data = document.cookie;
     let splits = data.split(",");
-    for(var i = 0; i < splits.length; i++){
+
+    for (var i = 0; i < splits.length; i++) {
+
         let thisOne = splits[i].trim();
         let tokens = thisOne.split("=");
-        if(tokens[0] == "firstName"){
+
+        if (tokens[0] == "firstName") {
             firstName = tokens[1];
         }
-        else if(tokens[0] == "lastName"){
+
+        else if (tokens[0] == "lastName") {
             lastName = tokens[1];
         }
-        else if(tokens[0] == "userId"){
+
+        else if (tokens[0] == "userId") {
             userId = parseInt(tokens[1].trim());
         }
     }
 
-    if(userId < 0){
-        window.location.href = "index.html"  
-    }
-    else{
-        document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+    if (userId < 0) {
+        window.location.href = "index.html";
     }
 }
 
